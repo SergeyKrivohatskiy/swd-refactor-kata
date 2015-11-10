e75e6fadfb0bf00001f571d30c51e33a0d874db6
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-11-10 09:52:35 +0300

    Task 2 implemented
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- test/task2/AbbreviateTest.java
+++ test/task2/AbbreviateTest.java
@@ -1,5 +1,30 @@
 package task2;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
 public class AbbreviateTest {
-  //Your tests go here
+    @Test
+    public void nullTest() {
+	assertNull(Abbreviate.abbreviate(null, 124));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void lowWidthTest() {
+	Abbreviate.abbreviate("", 3);
+    }
+
+    @Test
+    public void normalTest() {
+	assertEquals("1...", Abbreviate.abbreviate("12345678", 4));
+	assertEquals("1234...", Abbreviate.abbreviate("12345678", 7));
+    }
+
+    @Test
+    public void bigWidthTest() {
+	assertEquals("12345678", Abbreviate.abbreviate("12345678", 8));
+	assertEquals("12345678", Abbreviate.abbreviate("12345678", 12346));
+    }
 }
