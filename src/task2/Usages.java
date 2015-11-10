4e7134701e05f7d22bffdc25672f993d792bd522
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-11-10 09:53:17 +0300

    Task 2 usages fixed
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- src/task2/Usages.java
+++ src/task2/Usages.java
@@ -1,8 +1,8 @@
 package task2;
 
 public class Usages {
-  public static void main(String[] args) {
-    Abbreviate.abbreviate("London is a capital of Great Britain", 0, 9);
-    Abbreviate.abbreviate("London is a capital of Great Britain", 7, 1000000);
-  }
+    public static void main(String[] args) {
+	Abbreviate.abbreviate("London is a capital of Great Britain", 9);
+	Abbreviate.abbreviate("London is a capital of Great Britain", 1000000);
+    }
 }
